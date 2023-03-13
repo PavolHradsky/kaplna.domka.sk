6c26f2c22c15c1b83b7631172b56884d4e06d2c7
Pavol Hradský <71588191+PavolHradsky@users.noreply.github.com>
2023-03-13 18:46:35 +0100

    fixed logo

--- wp-content/themes/kaplna/header.php
+++ wp-content/themes/kaplna/header.php
@@ -15,10 +15,10 @@
                 <div class="line h-1 w-6 bg-[#FDD998] group-hover:bg-[#F5B971] rounded-full"></div>
                 <div class="line h-1 w-6 bg-[#FDD998] group-hover:bg-[#F5B971] rounded-full"></div>
             </div>
-            <h3 class="text-white text-xl text-center">„S každým sa zhováraj tak, aby si sa stal jeho priateľom.“<span class="text-sm">don&#160Bosco</span></h3>
-            <a href="<?php echo get_home_url() ?>"><img src="<?php echo wp_get_attachment_image_url(24) ?>" alt="logo kaplna" class="w-14 h-14"></a>
+            <h3 class="text-white text-xl text-center grow">„S každým sa zhováraj tak, aby si sa stal jeho priateľom.“<span class="text-sm">don&#160Bosco</span></h3>
+            <a class="h-14 aspect-square" href="<?php echo get_home_url() ?>"><img src="<?php echo wp_get_attachment_image_url(24) ?>" alt="logo kaplna"></a>
         </header>
-        <nav class="bg-[#85A392] absolute h-screen p-3 hidden"> <!--min-w-full-->
+        <nav class="bg-[#85A392] absolute h-screen p-3 hidden w-full sm:w-1/2"> <!--min-w-full-->
             <ul class="text-gray-800 text-xl capitalize flex flex-col gap-3">
             <?php
             // Get wordpress menu and do a custom output
